337deeed19543ca8bbffcedaf10fdc8ee3b9d7ba
davinder pal <davinderpal32@gmail.com>
2021-12-26 17:06:23 +0530

    d credtnails

--- models/departments.js
+++ models/departments.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
     const departments = sequelize.define("departments",
         {
-            department_id: { type: Sequelize.BIGINT, primaryKey: true, autoIncrement: true },
+            department_id: { type: DataTypes.UUID, primaryKey: true, default: sequelize.fn('uuid_generate_v4') },
             department_name: { type: DataTypes.STRING(500), allowNull: false },
             total_employee: { type: DataTypes.BIGINT, allowNull: false, defaultValue: "0" },
             blocked: { type: DataTypes.ENUM("0", "1"), allowNull: false, defaultValue: "0" }
