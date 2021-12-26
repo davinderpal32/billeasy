dd5f746ba6a60ffbe2c0e00ce38c67e06d53b4ae
davinder pal <davinderpal32@gmail.com>
2021-12-26 15:00:38 +0530

    database credetnails

--- models/index.js
+++ models/index.js
@@ -12,6 +12,11 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  console.log('database credetnials>>>>>>>>>.',{
+    database : config.database,
+    username : config.username,
+    password : config.password
+  })
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
